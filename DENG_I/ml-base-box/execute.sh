5cd26c0b4752d8ddeb36fe0f2438e6184a33c6d4
Erik Graf <erik.graf@gmail.com>
2020-05-15 10:02:27 +0200

    Corrected notebook path in execute.sh

--- DENG_I/ml-base-box/execute.sh
+++ DENG_I/ml-base-box/execute.sh
@@ -4,4 +4,7 @@ ENV_NAME=ML_Base
 
 echo "Use the following command to start the notebook:"
 echo "conda activate $ENV_NAME"
-echo "jupyter notebook bfh/ocr/ocr-with-tesseract/eval.ipynb"
+echo "conda install jupyter"
+echo "jupyter notebook DENG_I_ML/Notebooks/unsupervised_learning/Unsupervised_Learning_Introduction.ipynb"
+echo "Press CTRL-C to exit the script"
+exit
